9dbe5648bd183897ce5d4fe457cc8fe585f3e496
Sam Lamsma <shl4@calvin.edu>
2024-12-19 18:46:12 -0500

    added final games

---
+++ Pinball_Playground/src/app/catch/catch.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catch',
+  imports: [],
+  templateUrl: './catch.component.html',
+  styleUrl: './catch.component.css'
+})
+export class CatchComponent {
+
+}
---
+++ Pinball_Playground/src/app/runner/runner.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-runner',
+  imports: [],
+  templateUrl: './runner.component.html',
+  styleUrl: './runner.component.css'
+})
+export class RunnerComponent {
+
+}
